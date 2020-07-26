a522332cf82be54fb66479eae0d8e4cd3f9af4b9
Daniel Saputra <danielwetan.io@gmail.com>
2020-07-26 14:44:00 +0700

    feature: mysql and response helpers

---
+++ src/helpers/response.js
@@ -0,0 +1,14 @@
+module.exports = {
+  response: (res, status, data, statusCode) => {
+    const result = {};
+
+    result.data = data || '';
+    result.statusCode = statusCode || 200;
+    result.status = status === 'success' ? true : false;
+
+    return res.status(result.statusCode).json({
+      status: result.status,
+      body: result.data
+    })
+  }
+}
